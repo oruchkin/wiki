70eb057544a14c8bfc5868a2c059e2192cf51422
oruchkin <72818824+oruchkin@users.noreply.github.com>
2020-11-11 11:41:44 +0300

    Добавление app_name

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = "ecylcopedia"
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:title>", views.wikititle, name="wikititle"),
-    path("wiki/", views.search, name="search")
+    path("wiki/<str:title>", views.wikititle,name="wikititle"),
+    path("wiki/", views.search, name="search"),
+    path("newpage", views.newpage, name="newpage"),
 ]
