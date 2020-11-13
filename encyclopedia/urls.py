6a6b483fc4b39728772b8dce4aab395ecae6a6fa
oruchkin <72818824+oruchkin@users.noreply.github.com>
2020-11-13 18:55:11 +0300

    error 11

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path("wiki/<str:title>", views.wikititle, name="wikititle"),
     path("wiki/", views.search, name="search"),
     path("newpage", views.newpage, name="newpage"),
+<<<<<<< HEAD
     path("new-page", views.newpage, name="new page"),
     path("save-page", views.save_page, name="save_page"),
     ]
+=======
+]
+>>>>>>> parent of 77ae1c9... 41
