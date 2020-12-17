049c3f1ceb99b2f8e5f41c6f8bfb45f0ae9133aa
oruchkin <72818824+oruchkin@users.noreply.github.com>
2020-12-17 21:59:27 +0300

    markdown started

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,6 +5,17 @@ from django.urls import reverse
 
 from . import util
 import random
+import markdown2
+
+markdown2.markdown("*boo!*")  # or use `html = markdown_path(PATH)`
+u'<p><em>boo!</em></p>\n'
+
+from markdown2 import Markdown
+markdowner = Markdown()
+markdowner.convert("*boo!*")
+u'<p><em>boo!</em></p>\n'
+markdowner.convert("**boom!**")
+u'<p><strong>boom!</strong></p>\n'
 
 
 def index(request):
